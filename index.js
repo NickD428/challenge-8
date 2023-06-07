6688e957152e56454776ec64e56421e826fe71fa
Nick <chargerfan428@aol.com>
2023-06-06 19:15:21 -0700

    added questions array for image creation options

---
+++ index.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Questions array for image creation
+
+const questions = [
+ {
+    type: 'input',
+    name: 'text',
+    message: 'Please choose 3 characters for your logo',
+
+ },
+ {
+    type: 'input',
+    name: 'text-color',
+    message: 'Please choose a color keyword or hex code for your image text',
+ },
+ {
+    type: 'list',
+    name: 'shape',
+    message: 'What shape would you like your image to be?',
+    choices: ['Circle', 'Triangle', 'Square'],
+ },
+ {
+    type: 'input',
+    name: 'shape-color',
+    message: 'Please choose a color keyword or hex code for your shape color',
+ }
+]
