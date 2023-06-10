588ed946d7ce089cc3477d387012e415b4ab7abd
Nick <chargerfan428@aol.com>
2023-06-10 15:39:15 -0700

    changed functions and and added a switch statement for selecting shapes and color

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateImage = require('./generateImage');
+
 
 // Questions array for image creation
 const questions = [
  {
     type: 'input',
-    name: 'text',
+    name: 'imageText',
     message: 'Please choose 3 characters for your logo',
 
  },
  {
     type: 'input',
-    name: 'text-color',
+    name: 'textColor',
     message: 'Please choose a color keyword or hex code for your image text',
  },
  {
     type: 'list',
     name: 'shape',
     message: 'What shape would you like your image to be?',
-    choices: ['Circle', 'Triangle', 'Square'],
+    choices: ['circle', 'triangle', 'square'],
  },
  {
     type: 'input',
-    name: 'shape-color',
+    name: 'shapeColor',
     message: 'Please choose a color keyword or hex code for your shape color',
  }
 ]
 
+// Initializes the app
 function init() {
     inquirer
         .prompt(questions)
-        
-}
-
-init();
+        .then((answers) => {
+         const { imageText, textColor, shape, shapeColor } = answers;
+     
+         let shapeSVG = '';
+     
+         switch (shape) {
+           case 'circle':
+             shapeSVG = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+             break;
+           case 'rectangle':
+             shapeSVG = `<rect x="50" y="50" width="200" height="100" fill="${shapeColor}" />`;
+             break;
+           case 'triangle':
+             shapeSVG = `<polygon points="150,50 250,150 50,150" fill="${shapeColor}" />`;
+             break;
+           default:
+             console.log('Invalid shape.');
+             return;
+         }}}
