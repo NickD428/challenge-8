b5784f4b178841c81d3e88bd8346bf7d88bf167d
Nick <chargerfan428@aol.com>
2023-06-10 15:42:11 -0700

    changed switch case from rectangle to square for clarity

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function init() {
            case 'circle':
              shapeSVG = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
              break;
-           case 'rectangle':
+           case 'square':
              shapeSVG = `<rect x="50" y="50" width="200" height="100" fill="${shapeColor}" />`;
              break;
            case 'triangle':
@@ -50,4 +50,25 @@ function init() {
            default:
              console.log('Invalid shape.');
              return;
-         }}}
+         }
+     
+         const svgContent = `
+           <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+             ${shapeSVG}
+             <text x="50%" y="50%" fill="${textColor}" font-size="20px" dominant-baseline="middle" text-anchor="middle">${imageText}</text>
+           </svg>
+         `;
+     
+         fs.writeFile('logo.svg', svgContent, (err) => {
+           if (err) {
+             console.error(err);
+           } else {
+             console.log('Logo generated successfully!');
+           }
+         });
+       })
+       .catch((error) => {
+         console.error(error);
+       });
+      }
+   init()
